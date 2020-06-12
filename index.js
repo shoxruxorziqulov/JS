6bb7d9ef967e0ca3f826bdfcfa0434e89d759ef3
shoxruxorziqulov <64428425+shoxruxorziqulov@users.noreply.github.com>
2020-06-12 17:32:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+// function Car(name, model) {
+//   this.name = name;
+//   this.model = model;
+// }
+
+// var cheep = new Car("Cheep", "audi");
+
+// console.log(cheep);
+
+// class Car {
+//   constructor(name, model, price) {
+//     this.name = name;
+//     this.model = model;
+//     this.price = price;
+//   }
+
+//   info() {
+//     return `Mashina nomi ${this.name}, modeli ${this.model}, narxi ${this.price}`;
+//   }
+// }
+
+// const car = new Car("Spark", "Chevrolet", 20000);
+
+// // console.log(car);
+
+// console.log(car.info());
+
+// class film {
+//   constructor(title, year, language) {
+//     this.title = title;
+//     this.year = year;
+//     this.language = language;
+//   }
+
+//   static movie() {
+//     console.log("Movie");
+//   }
+
+//   info() {
+//     return `Film nomi ${this.title}, ishlangan yili ${this.year}, ovozlastirish tili ${this.language}`;
+//   }
+// }
+
+// const kino = new film("Titanik", 2001, "english");
+
+// console.log(kino);
+// console.log(kino.info());
+// console.log(film.movie());
+
+// class non {
+//   constructor(son, narx) {
+//     Object.assign(this, { son, narx });
+//   }
+
+//   static jami(son, narx) {
+//     return son * narx;
+//   }
+// }
+
+// console.log(non.jami(3, 4000));
